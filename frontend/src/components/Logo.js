0a0aab1f669d63678653418605d27aa68a4d1e6f
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-08-04 23:41:43 +0530

    Update Logo.js

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Logo = ({ w, h }) => {
   return (
     <Link to="/" className="flex items-center gap-3">
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" width={24} height={24}></img>
       <div className="hidden md:flex text-2xl tracking-wide font-semibold">
         <span className="text-red-600 font-bold">A2 </span>
         Ecommerce
