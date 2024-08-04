fc556d0b88d079f8b77a91d643a9e49fdcbb7f23
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-08-04 21:54:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,18 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// API routes here
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT
