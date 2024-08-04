07faaf4b9cd0aae5c11ad086b961e9a5d10e5cfd
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-08-04 23:35:52 +0530

    Update Logo.js

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import logo from "../assest/logo.png"
-import { Link } from'react-router-dom'
+import React from 'react';
+import logo from "../assest/logo.png";
+import { Link } from 'react-router-dom';
 
-const Logo = ({w,h}) => {
+const Logo = ({ w, h }) => {
   return (
-    <Link href="/" className="flex items-center gap-3">
-        <img src={logo} alt="logo" width={24} height={24}></img>
-        <div className="text-2xl tracking-wide font-semibold"><span className="text-red-600 font-bold">A2 </span>Ecommerce</div>
+    <Link to="/" className="flex items-center gap-3">
+      <img src={logo} alt="logo" className={`${w} ${h}`} />
+      <div className="hidden md:flex text-2xl tracking-wide font-semibold">
+        <span className="text-red-600 font-bold">A2 </span>
+        Ecommerce
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
