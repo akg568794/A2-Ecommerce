f14ee937ff3fda4ffdfa137e68c80794e5b65e75
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-08-06 22:13:42 +0530

    Update Success.js

--- frontend/src/pages/Success.js
+++ frontend/src/pages/Success.js
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom';
 
 const Success = () => {
   return (
-    <div className='bg-gray-200 w-full max-w-md mx-auto flex flex-col justify-center items-center p-4'>
-        <img className='' src={successImage} width={200} height={200}></img>
-        <p className='text-green-600 font-bold text-xl'>Payment Successfull</p>
-        <Link to="/order" className='p-2 px-3 rounded my-2 border-2 border-green-600 font-semibold text-green-600 hover:bg-green-600 hover:text-white'>See Order</Link>
+    <div className='flex h-[calc(100vh-120px)] w-full justify-center items-center'>
+      <div className='max-w-md flex flex-col justify-center items-center p-4'>
+        <img src={successImage} width={200} height={200} alt="Success"/>
+        <p className='text-green-600 font-bold text-xl'>Payment Successful</p>
+        <Link to="/order" className='p-2 px-3 rounded my-2 border-2 border-green-600 font-semibold text-green-600 hover:bg-green-600 hover:text-white'>
+          See Order
+        </Link>
+      </div>
     </div>
   )
 }
