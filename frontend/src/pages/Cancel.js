8fafd9c8d95f591fbb54d09446a79d0f007021e5
Abhay gupta <109415581+akg568794@users.noreply.github.com>
2024-08-06 22:13:06 +0530

    Update Cancel.js

--- frontend/src/pages/Cancel.js
+++ frontend/src/pages/Cancel.js
@@ -4,11 +4,15 @@ import cancelImage from "../assest/cancel.gif"
 
 const Cancel = () => {
     return (
-        <div className='bg-gray-200 w-full max-w-md mx-auto flex flex-col justify-center items-center p-4'>
-            <img className='' src={cancelImage} width={200} height={200}></img>
-            <p className='text-red-500 font-bold text-xl'>Payment Cancelled</p>
-            <Link to="/cart" className='p-2 px-3 rounded my-2 border-2 border-red-500 font-semibold text-red-600 hover:bg-red-500 hover:text-white'>Go To Cart</Link>
-        </div>
+    <div className='flex h-[calc(100vh-120px)] w-full justify-center items-center'>
+      <div className='max-w-md flex flex-col justify-center items-center p-4'>
+        <img src={cancelImage} width={200} height={200} alt="Success"/>
+        <p className='text-red-600 font-bold text-xl mt-4 mb-4'>Payment Cancelled</p>
+        <Link to="/cart" className='p-2 px-3 rounded my-2 border-2 border-red-600 font-semibold text-red-600 hover:bg-red-600 hover:text-white'>
+          Go To Cart
+        </Link>
+      </div>
+    </div>
       )
 }
 
